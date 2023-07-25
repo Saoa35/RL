845ffb4f698adbf19829f2be9e3e32b1a589dc25
Saoa35 <saoamah@gmail.com>
2023-07-25 16:44:43 +0400

    + env.d

--- src/App.tsx
+++ src/App.tsx
@@ -10,55 +10,55 @@ const SignUp = React.lazy(() => import("./pages/SignUp"));
 const TripPage = React.lazy(() => import("./pages/TripPage"));
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<Suspense>
-							<Home />
-						</Suspense>
-					}
-				/>
-				<Route
-					path="sign-up"
-					element={
-						<Suspense>
-							<SignUp />
-						</Suspense>
-					}
-				/>
-				<Route
-					path="sign-in"
-					element={
-						<Suspense>
-							<SignIn />
-						</Suspense>
-					}
-				/>
-				<Route
-					path="trip/:id"
-					element={
-						<Suspense>
-							<TripPage />
-						</Suspense>
-					}
-				/>
-				<Route
-					path="bookings"
-					element={
-						<Suspense>
-							<Bookings />
-						</Suspense>
-					}
-				/>
-				<Route path="*" element={<Navigate to="/" />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="sign-up"
+          element={
+            <Suspense>
+              <SignUp />
+            </Suspense>
+          }
+        />
+        <Route
+          path="sign-in"
+          element={
+            <Suspense>
+              <SignIn />
+            </Suspense>
+          }
+        />
+        <Route
+          path="trip/:id"
+          element={
+            <Suspense>
+              <TripPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="bookings"
+          element={
+            <Suspense>
+              <Bookings />
+            </Suspense>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
